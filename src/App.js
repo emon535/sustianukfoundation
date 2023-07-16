01d0065bdbfd82d1857a85b776c167898445c132
emon535 <emon535@gmail.com>
2023-07-16 21:12:51 +0100

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        Welcome to SUSTian UK Foundation 
-       
+
       </header>
     </div>
   );
